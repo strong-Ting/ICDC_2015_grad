5ca9ac64e591f830c3287722dde871a03d57198f
strong-ting <s8527502987@gmail.com>
2021-06-26 21:04:07 +0800

    -update gitignore

--- sim/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
+++
@@ -1,4 +0,0 @@
-1600251382 /home/lin/github/tsmc13_neg.v
-1601104571 /home/lin/github/CIC_2015_grad/sim/testfixture.v
-1601106002 /home/lin/github/CIC_2015_grad/syn/SET_syn.v
-1601105962 /home/lin/github/CIC_2015_grad/src/SET.v
